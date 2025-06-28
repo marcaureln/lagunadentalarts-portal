30be59e7f56af85549531ece48b52e1958ae9df8
Alex N'Guessan <alexmarcaureln@gmail.com>
2025-06-28 16:30:10 +0000

    Add digital upload page

--- app/components/TheNavbar.vue
+++ app/components/TheNavbar.vue
@@ -12,7 +12,7 @@
         <NuxtLink to="#" class="hover:text-blue-700">Contact</NuxtLink>
       </nav>
       <div class="flex gap-3">
-        <NuxtLink to="#" class="rounded-4xl bg-blue-700 px-4 py-2 text-xs text-white hover:bg-blue-800">
+        <NuxtLink to="/digital-upload" class="rounded-4xl bg-blue-700 px-4 py-2 text-xs text-white hover:bg-blue-800">
           Digital Upload
         </NuxtLink>
         <NuxtLink
