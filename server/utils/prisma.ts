77594befa538bdefd6272da563037c3081bb0701
Alex N'Guessan <alexmarcaureln@gmail.com>
2025-11-21 11:18:51 +0000

    Make sure to load env vars

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-const connectionString = `${process.env.DATABASE_URL}`
-const pool = new pg.Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-const prisma = new PrismaClient({ adapter })
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
