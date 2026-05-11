ec7d8ab6062f2393b62c2df5bb4d0c57673ad930
Alex N'Guessan <alexmarcaureln@gmail.com>
2026-05-11 00:26:13 +0000

    chore: add eslint rule to use requireUserSession over getUserSession

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-  files: ['**/*.vue'],
-  rules: {
-    'vue/html-self-closing': 'off',
-    'prettier/prettier': [
-      'error',
-      {},
-      {
-        usePrettierrc: true,
-      },
-    ],
+export default withNuxt(
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/html-self-closing': 'off',
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
+    },
   },
-}).prepend(eslintPluginPrettierRecommended);
+  {
+    files: ['server/**/*.ts'],
+    rules: {
+      'no-restricted-syntax': [
+        'warn',
+        {
+          selector: "CallExpression[callee.name='getUserSession']",
+          message:
+            'Use requireUserSession in server routes; getUserSession is for places where a null session is a valid outcome.',
+        },
+      ],
+    },
+  }
+).prepend(eslintPluginPrettierRecommended);
